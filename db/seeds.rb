0eeee21c9a7e40be0536e2a010c6783f2705ccc0
shannon-nabors <shannon.nabors@gmail.com>
2019-02-04 15:53:13 -0500

    done

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title:"Chain of Fools", released: true, release_year: 1968, artist_name: "Aretha Franklin", genre: "Soul")
+Song.create(title:"Money Won't Change You", released: true, release_year: 1968, artist_name: "Aretha Franklin", genre: "Soul")
+Song.create(title:"People Get Ready", released: true, release_year: 1968, artist_name: "Aretha Franklin", genre: "Soul")
